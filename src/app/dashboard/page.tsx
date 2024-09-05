6a73fa7c104276ec7035f603ef704c88b7b26ea4
MigVarona <migvaronag@gmail.com>
2024-09-05 21:08:28 +0200

    pr

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
-"use client"
-
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { Amae } from "@/components/Amae";
 import LogoutButton from "@/components/Logout";
 
@@ -15,16 +14,16 @@ export default function Dashboard() {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (!token) {
-      router.push('/');
+      router.push("/");
     } else {
       setLoading(false);
     }
   }, [router]);
 
   if (loading) {
-    return <div>Loading...</div>; 
+    return <div>Loading...</div>;
   }
 
   return (
@@ -33,21 +32,24 @@ export default function Dashboard() {
       <Amae />
       <section className="w-full bg-black py-12 md:py-24 lg:py-32 flex justify-center items-center">
         <div className="container grid grid-cols-2 gap-6 px-4 sm:grid-cols-2 md:grid-cols-4 lg:gap-8 justify-center items-center">
-          <div className="relative overflow-hidden rounded-lg">
-            <img
-              src="/logo_glitch1_rgb_blanco.jpg"
-              alt="Video 1"
-              width={600}
-              height={400}
-              className="aspect-video object-cover w-full transition-transform duration-300 ease-in-out group-hover:scale-105"
-            />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <h3 className="text-2xl font-bold text-white">Video 1</h3>
+          <a href="https://vimeo.com/890050563/88fcdcea98">
+            <div className="relative overflow-hidden rounded-lg">
+              <img
+                src="/masterclass_ia.png"
+                alt="Video 1"
+                width={600}
+                height={400}
+                className="aspect-video object-cover w-full transition-transform duration-300 ease-in-out group-hover:scale-105"
+              />
+
+              <div className="absolute inset-0 flex items-center justify-center">
+                <h3 className="text-2xl font-bold text-white">Video 1</h3>
+              </div>
             </div>
-          </div>
+          </a>
           <div className="relative overflow-hidden rounded-lg">
             <img
-              src="/logo_glitch1_rgb_blanco.jpg"
+              src="/masterclass_ia.png"
               alt="Video 2"
               width={600}
               height={400}
